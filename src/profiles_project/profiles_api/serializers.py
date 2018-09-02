60c839d9f6ace608ad0286089c776e901a2d4013
Antony King <antony@antonys-mbp.home>
2018-09-02 16:15:55 +0100

    add api view crud for hello api view

---
+++ src/profiles_project/profiles_api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """ Serializes a name field for testing our APIView. """
+
+    first_name = serializers.CharField(max_length=15)
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """ Test API View. """
+
+    serailizer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ Return a list of APIView features. """
@@ -19,3 +24,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """ Create a hello message with our name. """
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            first_name = serializer.data.get('first_name')
+
+            message = 'Hello {0}'.format(first_name)
+            return Response({'message': message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """ Handle updating an object. """
+
+        return Response({'Method': 'put'})
+
+    def patch(self, request, pk=None):
+        """ Patch request, only updates fields provided in the request. """ 
+
+        return Response({'method': 'patch'})
+
+    def delete (self, request, pk=None):
+        """ Deletes an object. """
+
+        return Response({'Method': 'delete'})
