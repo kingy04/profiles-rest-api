8793bd933e4dc91a3fb468de71132c67c9134449
Antony King <kingy04@hotmail.com>
2018-09-05 10:13:32 +0100

    user status api / permission changes

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -7,8 +7,9 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet)
+router.register('profile', views.UserProfileViewSet) # model viewsets dont need basenames
 router.register('login', views.LoginViewSet, base_name="login")
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
